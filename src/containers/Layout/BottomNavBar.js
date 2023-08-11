73c88614bf5ce8df192237fd1b590e11ac1b3543
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-08-11 22:46:46 +0630

    add condition to route by clicking plus btn

--- src/containers/Layout/BottomNavBar.js
+++ src/containers/Layout/BottomNavBar.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 function BottomNavBar() {
+  const router = useRouter()
+  const plusUrl = router.pathname === '/' ? '/transaction/create' : '/'
   return (
-    <div className='absolute bottom-0 left-0 w-full'>
+    <div className='absolute bottom-0 left-0 w-full' onClick={()=> router.push(plusUrl)}>
         <div className=' flex justify-center -mb-8'>
             <div className='bg-main w-14 aspect-square rounded-full text-center'>
             <i className='pi pi-plus text-white mt-4 text-2xl'/>
