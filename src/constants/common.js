3f2bf190568f1562813b7824664e8cb7c2fe8676
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-08-13 21:06:15 +0630

    adjust border and text colors

--- src/constants/common.js
+++ src/constants/common.js
@@ -3,24 +3,24 @@ export const months = ["January","February","March","April","May","June","July",
 export const currency = "MMK"
 
 export const transactionColorMapper = {
-    'debit': 'bg-egray-100',
-    'credit': 'bg-eblue-50'
+    'debit': '!bg-egray-100',
+    'credit': '!bg-eblue-50'
 }
 
 export const PaymentIconColorMapper = ( payment)=> {
     switch(payment) {
-        case 'Cash': return {icon: 'pi pi-money-bill', color: 'bg-egold-50'}
-        case 'Bank': return {icon: 'pi pi-credit-card', color: 'bg-egray-100'}
-        case 'Pay': return {icon: "pi pi-mobile", color: "bg-epink-50"}
-        default: return {icon: 'pi pi-money-bill', color: 'bg-egold-50'}
+        case 'Cash': return {icon: 'pi pi-money-bill', color: '!bg-egold-50 !border-egold-50 !text-black'}
+        case 'Bank': return {icon: 'pi pi-credit-card', color: '!bg-egray-100 !border-egray-100 !text-black'}
+        case 'Pay': return {icon: "pi pi-mobile", color: "!bg-epink-50 !border-epink-50 !text-black"}
+        default: return {icon: 'pi pi-money-bill', color: '!bg-egold-50 !border-egold-50 !text-black'}
 
     }
 }
 
 export const CategoryIconColorMapper = ( payment)=> {
     switch(payment) {
-        case 'Bills': return {icon: 'pi pi-money-bill', color: 'bg-blue-500'}
-        case 'Bank': return {icon: 'pi pi-credit-card', color: 'bg-egray-100'}
-        case 'Pay': return {icon: "pi pi-mobile", color: "bg-epink-50"}
+        case 'Bills': return {icon: 'pi pi-money-bill', color: '!bg-blue-500 !border-blue-500 !text-black'}
+        case 'Bank': return {icon: 'pi pi-credit-card', color: '!bg-egray-100 !border-egray-100 !text-black'}
+        case 'Pay': return {icon: "pi pi-mobile", color: "!bg-epink-50 !border-epink-50"}
     }
 }
