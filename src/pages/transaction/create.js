226a634d70d309f4fcd5100edad0c443bd72cf26
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-08-13 23:47:53 +0630

    add api call

--- src/pages/transaction/create.js
+++ src/pages/transaction/create.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputComponent, CalendarComponent, RadioList } from '../../components/TransactionCard/Form'
 import { userPaymentTypes, necessityTypes } from '../../constants/ComponentData'
 import { Button } from 'primereact/button'
 import { Controller, useForm} from 'react-hook-form'
+import axios from 'axios'
+import {Dialog} from 'primereact/dialog'
+import { useRouter } from 'next/router'
 
 function TransactionCreatePage() {
-  const {getValues, control, handleSubmit} = useForm()
+  const {getValues, control, handleSubmit, reset} = useForm({defaultValues: {}})
+  const [confirmVisible, setConfirmVisible] = useState(false)
+  const [apiData, setApiData] = useState({})
+  const router = useRouter()
+
   const onSubmit = () => {
-    console.log ('xxx getValues ', getValues())
-  }
+    axios.post('http://localhost:3005/transactions', {
+          ...getValues()
+      }).then(response => {
+      console.log('transactions posted:', response.data);
+      setApiData({status: 'success'})
+      setConfirmVisible(true)
+      }).catch(error => {
+        setApiData({status: 'fail'})
+        console.error('Error posting data:', error);
+        setConfirmVisible(true)
+      });
+    }
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -64,6 +81,23 @@ function TransactionCreatePage() {
           <Button label='Save' className='!py-2 absolute bottom-8 '/>
         </div>
       </form>
+
+      <Dialog header={apiData.status === 'success' ? "Success" : "Fail"} visible={confirmVisible} onHide={() => setConfirmVisible(false)}>
+          {
+            apiData.status === 'success' ? 
+                <div>  
+                  <p className="p-4"> Transaction has been created.</p>
+                    <div className='flex flex-row-reverse gap-4'>
+                      <Button icon="pi pi-plus" label='Create New' type="button"  onClick={()=>{ reset(); setConfirmVisible(false)}}/>
+                      <Button icon="pi pi-home" label='Go Home' type="button" onClick={()=> router.push('/')}/>
+                    </div>
+                  </div>
+                  : <>
+                    <p> Something went wrong.</p>
+                    <Button icon="pi pi-plus" label='Create New' type="button"  onClick={()=> reset()}/>
+                    </>
+                }
+      </Dialog>         
     </>
   )
 }
