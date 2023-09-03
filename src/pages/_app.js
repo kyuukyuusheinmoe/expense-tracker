1db2e3b0d9cf9b985c4a241dc1264d5cbb4072cd
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-09-03 09:28:22 +0630

    add redux thunk

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
+import { Provider } from 'react-redux';
 import 'primeicons/primeicons.css';
 import "primereact/resources/primereact.min.css";
 import '../styles/globals.scss'
 import "../styles/theme.css";   
+import store from '../redux/store';
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
  
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(<Provider store={store}><Component {...pageProps} /></Provider>)
 }
 
 export default MyApp
