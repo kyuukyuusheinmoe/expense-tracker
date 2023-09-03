1cb4b2fc05a60f0f9864355e8eb18a9d98093302
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-09-03 17:27:54 +0630

    add link for page

--- src/containers/Layout/BottomNavBar.js
+++ src/containers/Layout/BottomNavBar.js
@@ -5,15 +5,15 @@ function BottomNavBar() {
   const router = useRouter()
   const plusUrl = router.pathname === '/' ? '/transaction/create' : '/'
   return (
-    <div className='absolute w-full max-w-[1024px] mx-auto bottom-0' onClick={()=> router.push(plusUrl)}>
-        <div className=' flex justify-center -mb-8'>
+    <div className='absolute w-full max-w-[1024px] mx-auto bottom-0'>
+        <div className=' flex justify-center -mb-8' onClick={()=> router.push(plusUrl)}>
             <div className='bg-main w-14 aspect-square rounded-full text-center'>
-            <i className='pi pi-plus text-white mt-3 !text-2xl'/>
+              <i className='pi pi-plus text-white mt-3 !text-2xl'/>
             </div>
         </div>
         <div className='border shadow-md flex justify-between p-6'>
-            <i className='pi pi-home !text-2xl'/>
-            <i className='pi pi-wallet !text-2xl'/>
+            <i className='pi pi-home !text-2xl'onClick={()=> router.push("/")}/>
+            <i className='pi pi-wallet !text-2xl' onClick={()=> router.push('/account')}/>
         </div>
     </div>
   )
