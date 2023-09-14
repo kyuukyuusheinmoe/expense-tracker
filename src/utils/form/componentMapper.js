01b52c0447abdf52c8d85260928993dc867be5c2
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-09-14 16:44:36 +0630

    update import for Component in map

--- src/utils/form/componentMapper.js
+++ src/utils/form/componentMapper.js
@@ -1,9 +1,9 @@
-import { InputComponent, CalendarComponent, RadioList, DropDownComponent } from "../../components/Form"
+import dynamic from "next/dynamic"
 
 export const componentMapper = {
-    Input: <InputComponent/>,
-    Calendar: <CalendarComponent/>,
-    DropDown: <DropDownComponent/>,
-    RadioSelect: <RadioList/>
+    Input:{component: dynamic(()=> import ("../../components/Form/Input"))},
+    Calendar: {component: dynamic(()=> import ("../../components/Form/Calendar"))},
+    DropDown: {component: dynamic(()=> import ("../../components/Form/DropDown"))},
+    RadioSelect: {component: dynamic(()=> import ("../../components/Form/RadioList"))},
 
 }
