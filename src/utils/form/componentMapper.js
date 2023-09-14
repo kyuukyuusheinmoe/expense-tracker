73a11fec517b8a4ddeb5404f66aa9c2d36743d56
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-09-14 16:17:44 +0630

    create component Mapper

---
+++ src/utils/form/componentMapper.js
@@ -0,0 +1,9 @@
+import { InputComponent, CalendarComponent, RadioList, DropDownComponent } from "../../components/Form"
+
+export const componentMapper = {
+    Input: <InputComponent/>,
+    Calendar: <CalendarComponent/>,
+    DropDown: <DropDownComponent/>,
+    RadioSelect: <RadioList/>
+
+}
