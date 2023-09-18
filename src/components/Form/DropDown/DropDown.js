c7800b2c1dbe504ad5aeaa367cc9c55a893a4f58
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-09-18 15:15:39 +0630

    fix option label

--- src/components/Form/DropDown/DropDown.js
+++ src/components/Form/DropDown/DropDown.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Dropdown } from 'primereact/dropdown';
 
 
-function DropDownComponent({label, options, value, onChange, placeholder}) {
+function DropDownComponent({label, items, value, onChange, placeholder, displayKey}) {
   return (
     <div className="grid grid-cols-12">
         <label className='col-span-4 text-center text-md font-bold'>{label}</label>
-        <Dropdown value={value} onChange={(e) => onChange(e.value)} options={options} optionLabel="name" 
+        <Dropdown value={value} onChange={(e) => onChange(e.value)} options={items} optionLabel={displayKey}
                 placeholder={placeholder} className="col-span-8 w-full" />
     </div>
   )
