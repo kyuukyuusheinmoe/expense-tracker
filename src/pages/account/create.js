b9e88af3acba149f9a6f2f304c0a6ee0afbf6400
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-10-15 10:01:46 +0630

    update form components into Dynamic Form Element

--- src/pages/account/create.js
+++ src/pages/account/create.js
@@ -1,30 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import { InputComponent, CalendarComponent, RadioList, DropDownComponent } from '../../components/Form'
-import { userPaymentTypes, necessityTypes } from '../../constants/ComponentData'
+import React, { useState } from 'react'
 import { Button } from 'primereact/button'
-import { Controller, useForm, useWatch} from 'react-hook-form'
+import { FormProvider, useForm} from 'react-hook-form'
 import axios from 'axios'
 import {Dialog} from 'primereact/dialog'
 import { useRouter } from 'next/router'
 import LayoutWithHeader from '../../containers/Layout/LayoutWithHeader'
-import clsx from 'clsx'
 import { CreateAccountForm as components } from '../../constants/CreateAccountForm'
 import DynamicFormElement from '../../components/Form/DynamicFormElement/index';
 
 function AccountCreatePage() {
-  const {getValues, control, handleSubmit, reset, register, unregister} = useForm({defaultValues: {}})
+  const methods = useForm({defaultValues: {}})
   const [confirmVisible, setConfirmVisible] = useState(false)
   const [apiData, setApiData] = useState({})
   const router = useRouter()
-  const typeCheck = useWatch({control, name: "type"})
 
-  useEffect(() => {
-    if (typeCheck === "in") {
-      unregister("necessity");
-    } else {
-      register("necessity");
-    }
-  }, [register, unregister, typeCheck]);
+  const {getValues, control, handleSubmit, reset} = methods;
 
   const onSubmit = () => {
     axios.post('http://localhost:3005/transactions', {
@@ -41,15 +31,16 @@ function AccountCreatePage() {
     }
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className='overflow-scroll'>
-        <div className='p-4 grid gap-5'>
-            {components?.map ((component, index) => <DynamicFormElement key={index} {...component} />)}
-        </div>
-        <div className='flex flex-row-reverse p-4'>
-          <Button label='Save' className='!py-2 fixed bottom-2 '/>
-        </div>
-      </form>
-
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)} className='overflow-scroll'>
+          <div className='p-4 grid gap-5'>
+              {components?.map ((component, index) => <DynamicFormElement key={index} componentType={component.formProps.type} {...component} control={control} />)}
+          </div>
+          <div className='flex flex-row-reverse p-4'>
+            <Button label='Save' className='!py-2 fixed bottom-2 '/>
+          </div>
+        </form>
+      </FormProvider>
       <Dialog header={apiData.status === 'success' ? "Success" : "Fail"} visible={confirmVisible} onHide={() => setConfirmVisible(false)}>
           {
             apiData.status === 'success' ? 
