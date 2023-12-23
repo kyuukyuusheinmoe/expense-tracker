9a7064a95f2b2580b298751794bc4ecac93d34bd
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-12-23 11:16:32 +0630

    add number input component

--- src/utils/form/componentMapper.js
+++ src/utils/form/componentMapper.js
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic"
 
 export const componentMapper = {
     Input:{component: dynamic(()=> import ("../../components/Form/Input"))},
+    Number:{component: dynamic(()=> import ("../../components/Form/NumberInput"))},
     Calendar: {component: dynamic(()=> import ("../../components/Form/Calendar"))},
     DropDown: {component: dynamic(()=> import ("../../components/Form/DropDown"))},
     RadioSelect: {component: dynamic(()=> import ("../../components/Form/RadioList"))},
