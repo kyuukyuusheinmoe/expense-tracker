cf965e381e8bde4e00fac699c8ee89832c3dbc99
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-12-24 12:05:28 +0630

    add title and date in trxn card

--- src/components/Cards/TransactionCard.js
+++ src/components/Cards/TransactionCard.js
@@ -3,16 +3,21 @@ import { Button } from 'primereact/button'
 import clsx from 'clsx'
 import { IconColorMapper } from '../../utils/common'
 import { currency } from '../../constants/common'
+import moment from 'moment'
 
 function TransactionCard({item}) {
   return (
-        <div className={clsx(IconColorMapper(item.type)?.color, 'py-4 px-2 text-center items-center rounded-lg')}>
+        <div className={clsx(IconColorMapper(item.type)?.color, 'py-2 px-2 text-center items-center rounded-lg text-sm')}>
             <div className='flex justify-between items-center'>
               <div className='flex gap-2 items-center'>
-                <Button icon={IconColorMapper(item.category.label)?.icon} className={clsx(IconColorMapper(item.category.label)?.color, "!rounded-full !px-2 !py-1")} ><span className='ml-1 mt-1'>{item.category.label}</span></Button>
-                <Button icon={IconColorMapper(item.account.accountType)?.icon || ''} className={clsx(IconColorMapper(item.account.accountType)?.color, "!rounded-full !px-2 !py-1")}><span className='ml-1 mt-1'>{item.account.accountType}</span></Button>
+                <Button icon={IconColorMapper(item.category.label)?.icon} className={clsx(IconColorMapper(item.category.label)?.color, " !flex !items-center !rounded-full !px-2 !py-1 !text-sm")} ><span className='ml-1'>{item.category.label}</span></Button>
+                <Button icon={IconColorMapper(item.account.accountType)?.icon || ''} className={clsx(IconColorMapper(item.account.accountType)?.color, "!flex !items-center !rounded-full !px-2 !py-1 !text-sm")}><span className='ml-1'>{item.account.accountType}</span></Button>
               </div>
               <p className='text-enavy-700 font-bold'><span className={clsx(item.spentType === 'out' ? 'pi pi-minus text-error': 'pi pi-plus text-success', 'mr-2')}></span>{item?.amount && `${Intl.NumberFormat().format(item.amount)} ${currency}`} </p>
+            </div>
+            <div className='w-full flex justify-between text-gray-500'>
+              <div> {item.title} </div>
+              <div> {moment(item.createdAt).format('DD/MM/YYYY')} </div>
             </div>
         </div>
   )
