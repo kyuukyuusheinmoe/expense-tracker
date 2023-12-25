46ced042dd37c7742830f61b90c45b06af7b3cd6
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2023-12-25 15:12:04 +0630

    add bg

--- src/containers/Layout/BottomNavBar.js
+++ src/containers/Layout/BottomNavBar.js
@@ -4,13 +4,13 @@ import React from 'react'
 function BottomNavBar({onClickPlus}) {
   const router = useRouter()
   return (
-    <div className='absolute w-full max-w-[1024px] mx-auto bottom-0'>
+    <div className='fixed w-full max-w-[1024px] mx-auto bottom-0 z-50 '>
         <div className=' flex justify-center -mb-8' onClick={onClickPlus}>
             <div className='bg-main w-14 aspect-square rounded-full text-center'>
               <i className='pi pi-plus text-white mt-3 !text-2xl'/>
             </div>
         </div>
-        <div className='border shadow-md flex justify-between p-6'>
+        <div className='border shadow-md flex justify-between p-6 bg-white'>
             <i className='pi pi-home !text-2xl'onClick={()=> router.push("/")}/>
             <i className='pi pi-wallet !text-2xl' onClick={()=> router.push('/account')}/>
         </div>
